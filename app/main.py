f35383a9c108701fc8220ca4d4ea7858961beb08
Rahul <rb514617@gmail.com>
2026-08-04 16:41:27 +0530

    Add User and Post models

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .database import engine
+from . import models
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
     return {"message":"hello world"}
+
---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+    posts = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="posts")
