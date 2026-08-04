f7c49bf7379fe0c18de2c4c3974cadb88a053bf1
Rahul <rb514617@gmail.com>
2026-08-04 19:47:13 +0530

    Implement user registration endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
+from .users import router as user_router
 
 app = FastAPI()
+app.include_router(user_router)
 models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
